c247295eeeec74b82fbfbb15d05c4053ecef0838
JaneyJiang <ljiang2008@126.com>
2019-01-29 15:45:06 +0800

    add an advance method
    添加一种比较好的解法

--- Remove_K_Digits/Remove_K_Digits.py
+++ Remove_K_Digits/Remove_K_Digits.py
@@ -10,22 +10,33 @@ class Solution(object):
         """
         N = len(num)
         if k >= N:
-            return '0'
-        remove = list()
-        i =  0
-        cur = i
-        i +=1
-        while cur<N and i<N and k:
-            if num[cur] > num[i]:
-                remove.append(cur)
-                cur = i
-            else:
-                remove.append(i)
-                i+=1
+            return str(0)
+        k = N-k
+        res = list()
+        index = 0
+        while k>0:
+            pre_min = min(num[index:N-k+1])
+            slice_index = num[index:N-k+1].index(pre_min)
+            index = index+slice_index+1
+            res.append(pre_min)
             k-=1
-        #return [num[i] for i range(N) if i in remove]
-        return str(int(''.join([num[i] for i in range(N) if i not in remove])))
+        return str(int(''.join(res)))
 
-num = "1432219"
-k = 3
-Solution().removeKdigits(num,k)
+    def removeKdigitsAdvance(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        N = len(num)
+        if k >= N:
+            return str(0)
+        res = list()
+        for n in num:
+            while k and res and res[-1]>n:#这里必须使用循环，把之前比当前数字大的数都移除。
+                res.pop()
+                k -=1
+            res.append(n)
+        if k :#如果k>0则说明前面的:-k个就是我们要找的，-k:后面的数比之前大。
+            return str(int(''.join(res[:-k])))
+        return str(int(''.join(res)))
--- Remove_K_Digits/Remove_K_Digits_Test.py
+++ Remove_K_Digits/Remove_K_Digits_Test.py
@@ -7,16 +7,32 @@ class removeKDigitsTest(unittest.TestCase):
         num = "1432219"
         k = 3
         self.assertEqual(Solution().removeKdigits(num,k),'1219')
+        self.assertEqual(Solution().removeKdigitsAdvance(num,k),'1219')
     
     def test_left_zero_is_front(self):
         num = "10200"
         k = 1
         #'0200'should represent as '200'
         self.assertEqual(Solution().removeKdigits(num,k),'200')
+        self.assertEqual(Solution().removeKdigitsAdvance(num,k),'200')
+
     def test_remove_all(self):
         num = "10"
         k = 2
         self.assertEqual(Solution().removeKdigits(num,k),'0')
+        self.assertEqual(Solution().removeKdigitsAdvance(num,k),'0')
+
+    def test_increasing_list(self):
+        num = "112"
+        k = 1
+        self.assertEqual(Solution().removeKdigits(num,k),'11')
+        self.assertEqual(Solution().removeKdigitsAdvance(num,k),'11')
+
+    def test_(self):
+        num = "1234567890"
+        k = 9
+        self.assertEqual(Solution().removeKdigits(num,k),'0')
+        self.assertEqual(Solution().removeKdigitsAdvance(num,k),'0')
 
 if __name__ == "__main__":
     unittest.main()
